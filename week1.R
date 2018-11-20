4ca17b53554d33199fffa0f07e5a3bfaca4e54e6
Hwang Yoon Tae <32198272+dbsxo2995@users.noreply.github.com>
2018-11-20 13:04:40 +0900

    Update week1.R

--- week1.R
+++ week1.R
@@ -370,3 +370,35 @@ print(sum)
                        ifelse(mpg$total >= 25 ,"B",
                               ifelse(mpg$total >= 20, "C","D")))    
    table(mpg$grade)   
+
+#단일품목 판매 통계적 모의실험
+##################################################################
+#한 신물팔이 소년이 신문 1부에 150원으로 사서 300원에 판매한다.###
+#팔지못한 신문은 1부당 100원에 환불받으며, 돈을 벌고 있다. 이 때##
+#10부 단위로 신문을 받아온다고 할 때, 매일 몇부씩 받아오는것이 ###
+#최대의 이익이 될것인가? 다음은 100일동안 매일의 판매 부수를 조사#
+#하여 확률 표로 나타내었다.                                      #
+#X : 10 20 30 40 50 60 70 80 90 판매부수 #
+#P(x) 4 6  12 20 25 18 10  3  2 단위(%)  #
+
+
+result <- matrix(0, nrow = 9, ncol = 2)
+sum_profit <- matrix(0, nrow = 9, ncol = 1)
+colnames(result) <- c("구입","이익")
+F <- c(0,00, 0,04, 0.10, 0.22, 0.42, 0.67, 0.85, 0.95, 0.98, 1.00) #누적확률
+set.seed(12345)
+
+for(i in 1:9){
+  buy = 10 * i
+  for(day in 1:1000){
+    u = runif(1, 0, 1)
+    for(j in 1:9){
+      sale = 10 * j
+      if(u >= F[j]){if(buy >= sale) profit = ((150 * sale) - (50 * (buy - sale))) else profit = 150 * buy}
+    }
+  }
+  sum_profit[i] = sum_profit[i] + profit
+  result[i, 1] <- buy
+  result[i, 2] <- sum_profit[i]
+}
+result
